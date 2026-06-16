1b68ae99c33ccdeb3e1bda730998a8354b9820f7
Saksdhote <sakshidhote2@gmail.com>
2026-06-16 14:06:14 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Sakshi',
+    author_email='Sakshidhote2@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
